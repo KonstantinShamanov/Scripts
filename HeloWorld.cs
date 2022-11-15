bdf043bec23209225407beb9fcd4cb0eccefea79
KonstantinShamanov <118287688+KonstantinShamanov@users.noreply.github.com>
2022-11-15 20:18:40 +0500

    Add files via upload

---
+++ HeloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //команда напечатает привет мир.
+        print ("Hello, World") ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
